54b3f908772ae8d59d9d2e0d0520c4d694d954f8
abimon <eddyabby59@gmail.com>
2023-03-14 20:43:56 +0300

    major styles

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('content')
 <link href="{{asset('storage/static/css/style.css')}}" rel="stylesheet">
+<link href="{{asset('storage/static/css/style.min.css')}}" rel="stylesheet">
 <div class="bg-transparent" style="height:80px;"></div>
 <header class="navbar navbar-dark bg-dark flex-md-nowrap p-0 shadow">
   <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">{{Auth()->user()->name}}</a>
