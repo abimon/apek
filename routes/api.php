abac284613aea86c7d50af6da7c52046833ddce3
abimon <eddyabby59@gmail.com>
2023-03-29 20:09:32 +0300

    api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
@@ -18,4 +19,4 @@ use App\Http\Controllers\dataController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/read',[dataController::class, 'read']);
+Route::get('/read',[ApiController::class, 'read']);
